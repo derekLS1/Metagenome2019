0dfb3d1f519234e1ae829e4dd74782a028c932ef
derekLS1 <derek.lundberg@gmail.com>
2019-01-17 18:23:40 +0100

    Add files via upload

---
+++ MiSeqDemultiplexTrim515F_806R.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+#
+#  Reserve 1 CPUs for this job
+#$ -pe parallel 1
+#  Request 10G of RAM
+#$ -l h_vmem=10G
+#
+#  The name shown in the qstat output and in the output file(s). The
+#  default is to use the script name.
+# -N $name
+#
+#  The path used for the standard output stream of the job
+# -o /ebio/abt6_projects7/bacterial_strain_analysis/data/processed_DATA
+#
+# Merge stdout and stderr. The job will create only one output file which
+# contains both the real output and the error messages.
+# -j y
+#
+#  Use /bin/bash to execute this script
+#$ -S /bin/bash
+#
+#  Run job from current working directory
+#$ -cwd
+#
+#  Send email when the job begins, ends, aborts, or is suspended
+##$ -m beas
+
+merged=$1
+usearch=/ebio/abt6_projects7/bacterial_strain_analysis/code/usearch10
+
+#515 1-3-5
+rm "$merged"_515F1F3F5.fastq
+grep "^GAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGTA$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 21 -stripright 23  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F1F3F5.fastq
+rm "$merged"_temp*
+grep "^GAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGTAGT$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 21 -stripright 25  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F1F3F5.fastq
+rm "$merged"_temp*
+grep "^GAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGTAGTCA$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 21 -stripright 27  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F1F3F5.fastq
+rm "$merged"_temp*
+
+grep "^CTGAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGTA$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 23 -stripright 23  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F1F3F5.fastq
+rm "$merged"_temp*
+grep "^CTGAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGTAGT$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 23 -stripright 25  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F1F3F5.fastq
+rm "$merged"_temp*
+grep "^CTGAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGTAGTCA$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 23 -stripright 27  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F1F3F5.fastq
+rm "$merged"_temp*
+
+grep "^GACTGAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGTA$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 25 -stripright 23  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F1F3F5.fastq
+rm "$merged"_temp*
+grep "^GACTGAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGTAGT$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 25 -stripright 25  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F1F3F5.fastq
+rm "$merged"_temp*
+grep "^GACTGAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGTAGTCA$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 25 -stripright 27  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F1F3F5.fastq
+rm "$merged"_temp*
+
+#515 2-4-6
+rm "$merged"_515F2F4F6.fastq
+grep "^TGAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGT$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 22 -stripright 22  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F2F4F6.fastq
+rm "$merged"_temp*
+grep "^TGAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGTAG$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 22 -stripright 24  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F2F4F6.fastq
+rm "$merged"_temp*
+grep "^TGAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGTAGTC$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 22 -stripright 26  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F2F4F6.fastq
+rm "$merged"_temp*
+
+grep "^ACTGAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGT$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 24 -stripright 22  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F2F4F6.fastq
+rm "$merged"_temp*
+grep "^ACTGAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGTAG$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 24 -stripright 24  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F2F4F6.fastq
+rm "$merged"_temp*
+grep "^ACTGAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGTAGTC$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 24 -stripright 26  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F2F4F6.fastq
+rm "$merged"_temp*
+
+grep "^TGACTGAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGT$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 26 -stripright 22  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F2F4F6.fastq
+rm "$merged"_temp*
+grep "^TGACTGAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGTAG$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 26 -stripright 24  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F2F4F6.fastq
+rm "$merged"_temp*
+grep "^TGACTGAGTG[ACTG]CAGC[ATCG]GCCGCGGTAA[ATCG]*ATTAGA[ATCG]ACCC[ATCG][ATCG]GTAGTCCGTAGTC$" $merged -B 1 -A 2 --no-group-separator > "$merged"_temp.fq 
+$usearch -fastx_truncate "$merged"_temp.fq -stripleft 26 -stripright 26  -fastqout "$merged"_temp.fq.stripped
+cat "$merged"_temp.fq.stripped >> "$merged"_515F2F4F6.fastq
+rm "$merged"_temp*
+
+
